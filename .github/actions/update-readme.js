b0c02e59b29d6b9d8f2f435a887645555b11e415
k66 <k66inthesky@gmail.com>
2023-10-20 22:15:03 +0800

    Update update-readme.js

--- .github/actions/update-readme.js
+++ .github/actions/update-readme.js
@@ -1,23 +1,17 @@
 const fs = require('fs');
-const { resolve } = require('path');
 
-const readmePath = resolve(__dirname, '..', 'README.md');
+let readmeContent = fs.readFileSync('README.md', 'utf-8');
 
-let readmeContent = fs.readFileSync(readmePath, 'utf8');
+const labelCntRegex = /{label_feed_cnt}/g;
 
-const label_feed_match = readmeContent.match(/{{ label_feed_cnt }}\s*:\s*([\d]+)/i);
+const currentCnt = parseInt(readmeContent.match(labelCntRegex)[0].replace(/\D/g, ''));
 
-let label_feed_cnt = 0;
+const newCnt = currentCnt + 1;
 
-if (label_feed_match && label_feed_match[1]) {
-    label_feed_cnt = parseInt(label_feed_match[1], 10);
-}
+readmeContent = readmeContent.replace(labelCntRegex, newCnt);
 
-label_feed_cnt++;
+fs.writeFileSync('README.md', readmeContent);
 
-readmeContent = readmeContent.replace(/{{ label_feed_cnt }}\s*:\s*([\d]+)/i, `{{ label_feed_cnt }}: ${label_feed_cnt}`);
+console.log(`label_feed_cnt 已更新為: ${newCnt}`);
 
-fs.writeFileSync(readmePath, readmeContent);
-
-console.log(`label_feed_cnt: ${label_feed_cnt}`);
 
