f566d227a5d8645a3a722e5aefb750d67da4b918
k66 <k66inthesky@gmail.com>
2023-10-20 19:04:23 +0800

    Update update-readme.js

--- .github/actions/update-readme.js
+++ .github/actions/update-readme.js
@@ -3,23 +3,21 @@ const { resolve } = require('path');
 
 const readmePath = resolve(__dirname, '..', 'README.md');
 
-const label_feed_cnt = getFeedCount();
+let readmeContent = fs.readFileSync(readmePath, 'utf8');
 
-const readmeContent = `
-![Feed Image](.github/images/feed.png)
-{{ label_feed_cnt }}
+const label_feed_match = readmeContent.match(/{{ label_feed_cnt }}\s*:\s*([\d]+)/i);
 
-`;
+let label_feed_cnt = 0;
+
+if (label_feed_match && label_feed_match[1]) {
+    label_feed_cnt = parseInt(label_feed_match[1], 10);
+}
+
+label_feed_cnt++;
+
+readmeContent = readmeContent.replace(/{{ label_feed_cnt }}\s*:\s*([\d]+)/i, `{{ label_feed_cnt }}: ${label_feed_cnt}`);
 
 fs.writeFileSync(readmePath, readmeContent);
 
-function getFeedCount() {
-    let label_feed_cnt = 0; // initial as 0
-    increaseFeedCount(label_feed_cnt);
-    return `Feed Count: `{{label_feed_cnt}}`;
-}
+console.log(`label_feed_cnt: ${label_feed_cnt}`);
 
-function increaseFeedCount(currentCount) {
-    const newCount = currentCount + 1;
-    return newCount;
-}
