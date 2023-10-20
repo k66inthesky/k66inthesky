ecf1da34a4b2c59ef0b5f73d3f61ce74429aa241
k66 <k66inthesky@gmail.com>
2023-10-20 22:36:02 +0800

    Update update-readme.js

--- .github/actions/update-readme.js
+++ .github/actions/update-readme.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+try{
 let readmeContent = fs.readFileSync('README.md', 'utf-8');
 
 const labelCntRegex = /{label_feed_cnt}/g;
@@ -13,5 +14,9 @@ readmeContent = readmeContent.replace(labelCntRegex, newCnt);
 fs.writeFileSync('README.md', readmeContent);
 
 console.log(`label_feed_cnt 已更新為: ${newCnt}`);
+} catch (error){
+  console.error('發生錯誤: ', error);
+  process.exit(1);
+}
 
 
